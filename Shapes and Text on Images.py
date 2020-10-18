a28ee5108d9eec81edc155e4ffc6bceb20e8f7b1
Yasiru Randika <43146170+YasiruRandika@users.noreply.github.com>
2020-10-18 23:28:50 +0530

    Create Shapes and Text on Images.py

---
+++ Shapes and Text on Images.py
@@ -0,0 +1,30 @@
+from builtins import int
+
+import cv2
+import numpy as np
+
+img = np.zeros((512, 512, 3), np.uint8)
+
+# Color the whole image with Blue
+img[:] = 250, 0, 0
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+
+# Create line on image
+cv2.line(img, (0, 0), (250, 250), (255, 255, 255), 3)
+
+cv2.imshow("Line on Image", img)
+cv2.waitKey(0)
+
+# Create line on image from stat to end
+cv2.line(img, (0, 0), (img.shape[1], img.shape[0]), (0, 0, 255), 3)
+
+cv2.imshow("Diagonal Line on Image", img)
+cv2.waitKey(0)
+
+# Rectangle on Image
+
+cv2.rectangle(img, (300, 10), (200, 100), (232, 33, 122), 2)
+cv2.imshow("Rectangle on Image", img)
+cv2.waitKey(0)
