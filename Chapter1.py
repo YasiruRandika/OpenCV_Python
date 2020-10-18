e4cdb6301de6a3739fc04864c4e288a9099f7b2a
Yasiru Randika <43146170+YasiruRandika@users.noreply.github.com>
2020-10-18 22:35:36 +0530

    Read Video from Web Cam

--- Chapter1.py
+++ Chapter1.py
@@ -6,3 +6,27 @@ img = cv2.imread("Resources/lenna.png")
 # Display the imported image
 cv2.imshow("Lenna Image", img)
 cv2.waitKey(0)
+
+# import Video
+cap = cv2.VideoCapture("Resources/video.mp4")
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Video using web cam
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+cap.set(10, 100)
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
