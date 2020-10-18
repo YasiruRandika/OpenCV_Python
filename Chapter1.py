35f78933da1d869e30950830275751b722269809
Yasiru Randika <43146170+YasiruRandika@users.noreply.github.com>
2020-10-18 23:03:12 +0530

    Image Color Transformation

--- Chapter1.py
+++ Chapter1.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # import image for the project
 img = cv2.imread("Resources/lenna.png")
@@ -30,3 +31,20 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+# Image color Transformation
+img = cv2.imread("Resources/lenna.png")
+kernel = np.ones((5, 5), np.uint8)
+
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray, (7,7), 0)
+imgCanny = cv2.Canny(img, 100, 200)
+imgDilation = cv2.dilate(imgCanny, kernel, iterations=1)
+imgEroded = cv2.erode(imgDilation, kernel, iterations=1)
+
+cv2.imshow("Gray Image", imgGray)
+cv2.imshow("Blur Image", imgBlur)
+cv2.imshow("Canny Image", imgCanny)
+cv2.imshow("Dilation Image", imgDilation)
+cv2.imshow("Erotion Image", imgEroded)
+cv2.waitKey(0)
