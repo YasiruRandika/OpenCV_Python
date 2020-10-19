ac8b5158ebd9c733b5706de5aef5e68fc945cc8e
Yasiru Randika <43146170+YasiruRandika@users.noreply.github.com>
2020-10-19 20:29:35 +0530

    Update VirtualPaint.py

--- Face Detection.py
+++ Face Detection.py
@@ -1,14 +1,22 @@
 import cv2
 
-faceCascade = cv2.CascadeClassifier("Resour")
+faceCascade = cv2.CascadeClassifier("Resources/haarcascade_frontalface_default.xml")
 
-img = cv2.imread("Resources/lenna.png")
-imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# img = cv2.imread("Resources/lenna.png")
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
 
-faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
+while True:
+    success, img = cap.read()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
 
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-cv2.imshow("Result", img)
-cv2.waitKey(0)
+    cv2.imshow("Result", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
